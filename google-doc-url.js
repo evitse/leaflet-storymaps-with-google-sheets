58a7129a777a1c9ca508acd3ac697819f56f2fdc
evitse <41148265+evitse@users.noreply.github.com>
2018-10-26 10:55:29 +0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1gSoz8l8fiXCqoVIsZEiyrmCo1zACwMb5KeEWVyE-HZQ/edit#gid=0';
